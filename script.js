d08a253004e10977833a7dd4936192efadc4fe68
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-12 20:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toCase(text) {
   // write your code here
+	// Check for empty string
+  if (text === '') {
+    return '-';
+  }
+
+  // Convert to lowercase and uppercase, then concatenate with '-'
+  const result = `${text.toLowerCase()}-${text.toUpperCase()}`;
+
+  return result;
 }
 
 // DO not change the code below
